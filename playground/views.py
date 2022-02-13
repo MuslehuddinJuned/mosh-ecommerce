72a9b7e7bd73eb7e7d5958dcee6db456f4a6ab24
Muslehuddin Juned <41875815+MuslehuddinJuned@users.noreply.github.com>
2022-02-13 18:45:56 +0600

    end of 2nd Chapter

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def say_hello(request):
+    # return HttpResponse('Welcome')
+    name = 'Juned'
+    return render(request, 'hello.html', {'name': name})
